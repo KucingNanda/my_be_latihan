976115a2438c5f270d11cb5e65b8af71fc2ae492
KucingNanda <nandasr.24@gmail.com>
2026-06-22 15:41:03 +0700

    cors fe

--- config/cors.go
+++ config/cors.go
@@ -3,6 +3,7 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:5173",
 	"https://mybelatihan-production.up.railway.app",
+	"https://my-fe-latihan.vercel.app",
 }
 
 func GetAllowedOrigins() []string {
